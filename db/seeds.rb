af8c25471b92d67382e8c750456f4324fbe37d88
LEARN Academy <learnacademy@MacBook-Air-6.local>
2024-01-09 21:29:02 -0800

    added more examples in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ user3 = User.where(email: "test2@example.com").first_or_create(password: "passwo
 recipes = [
     {
         name: "steak",
-        image:"https://media.istockphoto.com/id/1371751060/photo/,grilled-medium-rare-top-sirloin-beef-steak-or-rump-steak-on-a-steel-tray-dark-background-top.jpg?s=612x612&w=0&k=20&c=svqnTZV_l7DP0QPCG8L_-f6k7LuBUA-cH9wiL8eJqUs=",
+        image:"https://img.freepik.com/free-photo/juicy-steak-medium-rare-beef-with-spices-grilled-vegetables_24972-2328.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1704672000&semt=sph",
         instructions:"Add the oil to the pan and heat until it begins to shimmer and move fluidly around the pan.",
         protein:"steak",
         ingredients:"steak, oil, butter",
@@ -27,6 +27,22 @@ recipes = [
         ingredients:"salt and pepper the salmon",
         nutrition:"25 grams of protein",
     },
+    {
+        name: "Pan-Fried Sesame Garlic Tofu",
+        image: "https://www.tablefortwoblog.com/wp-content/uploads/2018/09/pan-fried-spicy-garlic-tofu-recipe-photos-tablefortwoblog-1-scaled.jpg.webp",
+        instructions:"1. Place two paper towels on a plate then place the entire block of tofu (drained from its packaging liquid) onto the plate. Place two more paper towels on top of the tofu then put a heavy item on top. Not TOO heavy that it would completely crush the tofu but heavy enough that it can squeeze out liquid.2. Leave the tofu to drain for 30 minutes and halfway through, change out the paper towels. 3. ALTERNATIVELY and more eco-friendly: place a wire baking rack on top of a rimmed baking sheet. Place tofu on top and then put a heavy object on top and let drain for 30 minutes. 4. Once the tofu has drained, slice in half then into cubes and place into a large bowl. Sprinkle cornstarch on top and gently toss with your hands to ensure all tofu pieces are covered. 5. In a large skillet, add sesame oil and vegetable oil then bring up to medium-high heat. 6. Once oil is heated up, add the tofu to the skillet and let brown on all sides. 7. In the meantime, whisk together the ingredients for the sauce. 8. Once the tofu has browned and crisped up on all sides, add the sauce to the skillet and it should start to thicken immediately. Toss the tofu around to coat then remove from heat.9. Serve and enjoy!",
+        protein:"tofu",
+        ingredients:"cornstarch, sesame oil, vegetable oil, low-sodium soy sauce, water, honey, sambal oelek, garlic paste, rice wine vinegar",
+        nutrition:"1 SERVING | Calories: 313 KCAL | Carbohydrates: 17 G | Protein: 19 G | Fat: 16 G",
+    },
+    {
+        name: "Egg-celent Breakfast Muffins",
+        image: "https://fruitsandveggies.org/wp-content/uploads/2023/11/shutterstock_1445166449-masthead.jpg",
+        instructions: "1. Preheat the oven to 350 degrees. Prepare the muffin tin with non-stick spray. 2. Wash your hands with soap and water. 3. Crack the eggs into a large bowl. Then wash your hands with soap and water again. 4. In the large bowl, whisk together the eggs, milk, pepper, and paprika until well combined. 5. Add the broccoli, red bell pepper, orange bell pepper, and cheddar cheese to the bowl. Stir to combine. 6. Using a ¼ cup measure, fill each muffin spot so that it is about ¾ of the way full. 7. Bake for 20 minutes, or until the eggs are set. 8. Allow to cool slightly before using a spatula or butter knife to remove the egg bites from the muffin tin. And enjoy!",
+        protein:"Eggs",
+        ingredients:"Oil, Milk, Pepper, Praprika, Brocolli, Red Bel Pepper, Orange Bell Pepper, Cheddar Cheese",
+        nutrition:"9999999 grams of protein",
+    },
 ]
 
 recipes.each do |each_recipe|
@@ -36,28 +52,52 @@ end
 
 user1_reviews = [
     {
+        id: 1,
         header:"Taste delicious",
         body:"the pasta was very creamy",
         stars:3,
         recipe_id:1,
-        
+    },
+    {
+        id: 2,
+        header:"Taste delicious",
+        body:"the pasta was very creamy",
+        stars:3,
+        recipe_id:2,
     },
 ]
 user2_reviews = [ 
     {
+        id: 3,
         header:"It was not that great",
         body:"the steak was over cooked",
         stars:1,
         recipe_id:2,
     },
+    {
+        id: 4,
+        header:"Taste delicious",
+        body:"the pasta was very creamy",
+        stars:3,
+        recipe_id:3,
+    },
 ]
 user3_reviews = [   
     {
+        id: 5,
         header:"Best food ive had in a long time",
         body:"would come back again!",
         stars:5,
         recipe_id:3,
     },
+    {
+        id: 6,
+        header:"Taste delicious",
+        body:"the pasta was very creamy",
+        stars:3,
+        recipe_id:1,
+    },
+    
 ]
 
 
